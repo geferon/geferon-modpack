ea6fba139771053fde5c8d4aa5bec6bed5f1c27f
Geferon <me@geferon.net>
2026-08-30 17:07:18 +0200

    Added missing mod, removed conflicting mod

--- kubejs/server_scripts/modpack/mini_portals.js
+++ kubejs/server_scripts/modpack/mini_portals.js
@@ -1,5 +1,8 @@
 ServerEvents.recipes(allthemods => {
-    // Mini Nether
+    // Note: The following recipes are disabled because the required items from allthetweaks mod don't exist
+    // Uncomment and replace item IDs if you have the mod installed or want to use alternative items
+    
+    /* Mini Nether - DISABLED
     allthemods.shaped('allthetweaks:mini_nether', ['ADA', 'BCB', 'AEA'], {
         A: 'minecraft:obsidian',
         B: 'minecraft:nether_star',
@@ -8,18 +11,19 @@ ServerEvents.recipes(allthemods => {
         E: 'minecraft:warped_nylium'
     }).id('allthemods:allthetweaks/mini_nether')
 
-    // Mini End
+    // Mini End - DISABLED
     allthemods.shaped('allthetweaks:mini_end', ['ABA', 'BCB', 'ABA'], {
         A: 'apothic_enchanting:endshelf',
         B: 'minecraft:ender_eye',
         C: 'apothic_enchanting:draconic_endshelf'
     }).id('allthemods:allthetweaks/mini_end')
 
-    // Mini Exit
+    // Mini Exit - DISABLED
     allthemods.shaped('allthetweaks:mini_exit', ['ABA', 'DCD', 'ADA'], {
         A: 'minecraft:dragon_egg',
         B: 'apothic_enchanting:infused_breath',
         C: 'apothic_enchanting:soul_touched_sculkshelf',
         D: 'minecraft:end_crystal'
     }).id('allthemods:allthetweaks/mini_exit')
+    */
 })
