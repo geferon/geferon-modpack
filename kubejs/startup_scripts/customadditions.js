cac1487eb4f625807a99b311c2153f37b3911d84
Geferon <me@geferon.net>
2026-09-05 01:03:10 +0200

    Removed unused items

--- kubejs/startup_scripts/customadditions.js
+++ kubejs/startup_scripts/customadditions.js
@@ -14,31 +14,9 @@ StartupEvents.registry('block', allthemods => {
 })
 
 StartupEvents.registry('item', allthemods => {
-    allthemods.create('starlight_prediction').displayName('Generalized Starlight Prediction')
-
     allthemods.create('silent_allthemodium_plate').displayName('Silent Allthemodium Plate').glow(true);
     allthemods.create('silent_vibranium_plate').displayName('Silent Vibranium Plate').glow(true);
     allthemods.create('silent_unobtainium_plate').displayName('Silent Unobtainium Plate').glow(true);
-
-    allthemods.create('atm_star_shard_1').displayName('ATM Star Fragment')
-    allthemods.create('atm_star_shard_2').displayName('ATM Star Fragment')
-    allthemods.create('atm_star_shard_3').displayName('ATM Star Fragment')
-    allthemods.create('atm_star_shard_4').displayName('ATM Star Fragment')
-    allthemods.create('atm_star_shard_5').displayName('ATM Star Fragment')
-    allthemods.create('modularium_plate').color(0xe64200).texture('alltheores:item/iron_plate')
-
-    allthemods.create('inferium_enchanting_base').texture('minecraft:item/enchanted_book').color(0x55FF55)
-    allthemods.create('prudentium_enchanting_base').texture('minecraft:item/enchanted_book').color(0x00AA00)
-    allthemods.create('tertium_enchanting_base').texture('minecraft:item/enchanted_book').color(0xEB7114)
-    allthemods.create('imperium_enchanting_base').texture('minecraft:item/enchanted_book').color(0x5555FF)
-    allthemods.create('supremium_enchanting_base').texture('minecraft:item/enchanted_book').color(0xFF5555)
-
-    allthemods.create('fire_eye').use((level, player, hand) => global.iceAndFirePearls(level, player, hand)).texture('kubejs:item/fire_eye')
-    allthemods.create('fire_pearl').texture('kubejs:item/fire_pearl')
-    allthemods.create('lightning_eye').use((level, player, hand) => global.iceAndFirePearls(level, player, hand)).texture('kubejs:item/lightning_eye')
-    allthemods.create('lightning_pearl').texture('kubejs:item/lightning_pearl')
-    allthemods.create('ice_eye').use((level, player, hand) => global.iceAndFirePearls(level, player, hand)).texture('kubejs:item/ice_eye')
-    allthemods.create('ice_pearl').texture('kubejs:item/ice_pearl')
 
     allthemods.create("allthemodium_solar_sail_package").displayName("Allthemodium Solar Sail Package").component("dysoncubeproject:sphere_component_solar_sail", Java.cast("java.lang.Integer", 64))
     allthemods.create("allthemodium_beam_package").displayName("Allthemodium Beam Package").component("dysoncubeproject:sphere_component_beam", Java.cast("java.lang.Integer", 16))
