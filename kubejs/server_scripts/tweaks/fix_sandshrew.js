a4e0d191ac31ffc3015170128eb8ecb934846b7d
Jorge Mariño Mariño <jorge.marino@merkator.com>
2026-09-16 12:03:33 +0200

    Fix ban #3

--- kubejs/server_scripts/tweaks/fix_sandshrew.js
+++ kubejs/server_scripts/tweaks/fix_sandshrew.js
@@ -4,13 +4,11 @@ EntityEvents.spawned((event) => {
   const { entity } = event;
   if (entity.type != "cobblemon:pokemon") return;
 
-  const mobId = entity.nbt.Pokemon?.species;
-
-  // 2. The List of Banned Mobs
   const bannedMobs = ["cobblemon:sandshrew", "cobblemon:sandshrew-alola"];
 
-  if (bannedMobs.includes(mobId)) {
-    // console.log(`[DEBUG-SPAWN] BLOCKED: ${mobId} is on the ban list.`)
+  const species = entity.nbt.Pokemon?.Species;
+
+  if (bannedMobs.includes(species)) {
     event.cancel();
   }
 });
